2592e99e9ef6ea79f94021e4078394d457032ab0
tech-1234 <ankitdutta1741@gmail.com>
2024-06-22 16:49:44 +0530

    Add get user channel profile endpoint and fixed bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -366,7 +366,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                     $size: "$subscribedTo"
                 },
                 isSubscribed: {
-                    $condition: {
+                    $cond: {
                         if: { $in: [req.user?._id, "$subscribers.subscriber"] },
                         then: true,
                         else: false
